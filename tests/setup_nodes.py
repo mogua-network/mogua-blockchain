8549448f805638cd7a66e3aaa23ea6049fdd5ce9
mogua <mogua@gmail.com>
2021-08-22 08:51:09 +0800

    0822-1

--- tests/setup_nodes.py
+++ tests/setup_nodes.py
@@ -207,10 +207,10 @@ async def setup_farmer(
     config = bt.config["farmer"]
     config_pool = bt.config["pool"]
 
-    config["xfx_target_address"] = encode_puzzle_hash(b_tools.farmer_ph, "xfx")
+    config["mga_target_address"] = encode_puzzle_hash(b_tools.farmer_ph, "mga")
     config["pool_public_keys"] = [bytes(pk).hex() for pk in b_tools.pool_pubkeys]
     config["port"] = port
-    config_pool["xfx_target_address"] = encode_puzzle_hash(b_tools.pool_ph, "xfx")
+    config_pool["mga_target_address"] = encode_puzzle_hash(b_tools.pool_ph, "mga")
 
     if full_node_port:
         config["full_node_peer"]["host"] = self_hostname
