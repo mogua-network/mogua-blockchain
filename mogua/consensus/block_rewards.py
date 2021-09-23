6704f8796a783a2a5ecc2930f951cb5e8b9b4547
mogua <mogua@gmail.com>
2021-09-23 14:04:02 +0800

    0923-1

--- mogua/consensus/block_rewards.py
+++ mogua/consensus/block_rewards.py
@@ -15,15 +15,13 @@ def calculate_pool_reward(height: uint32) -> uint64:
     """
 
     if height == 0:
-        return uint64(int((7 / 8) * 21000000 * _mojo_per_mogua))
+         return uint64(int((7 / 8) * 21000000 * _mojo_per_mogua))
     elif height < ( 1/12 ) * _blocks_per_year:
-        return uint64(int((7 / 8) * 30 * _mojo_per_mogua))    #The first Mogua community reward
-    elif height < ( 1/10 ) * _blocks_per_year:
-        return uint64(int((7 / 8) * 20 * _mojo_per_mogua))    #The first Mogua community reward
+        return uint64(int((7 / 8) * 10 * _mojo_per_mogua))     #The first Mogua community reward
     elif height < 1 * _blocks_per_year:
-        return uint64(int((7 / 8) * 6 * _mojo_per_mogua))
-    elif height < 2 * _blocks_per_year:
         return uint64(int((7 / 8) * 4 * _mojo_per_mogua))
+    elif height < 2 * _blocks_per_year:
+        return uint64(int((7 / 8) * 3 * _mojo_per_mogua))
     elif height < 3 * _blocks_per_year:
         return uint64(int((7 / 8) * 2 * _mojo_per_mogua))
     elif height < 6 * _blocks_per_year:
@@ -48,13 +46,11 @@ def calculate_base_farmer_reward(height: uint32) -> uint64:
     if height == 0:
         return uint64(int((1 / 8) * 21000000 * _mojo_per_mogua))
     elif height < ( 1/12 ) * _blocks_per_year:
-        return uint64(int((1 / 8) * 30 * _mojo_per_mogua))     #The first Mogua community reward
-    elif height < ( 1/10 ) * _blocks_per_year:
-        return uint64(int((1 / 8) * 20 * _mojo_per_mogua))     #The first Mogua community reward
+        return uint64(int((1 / 8) * 10 * _mojo_per_mogua))     #The first Mogua community reward
     elif height < 1 * _blocks_per_year:
-        return uint64(int((1 / 8) * 6 * _mojo_per_mogua))
-    elif height < 2 * _blocks_per_year:
         return uint64(int((1 / 8) * 4 * _mojo_per_mogua))
+    elif height < 2 * _blocks_per_year:
+        return uint64(int((1 / 8) * 3 * _mojo_per_mogua))
     elif height < 3 * _blocks_per_year:
         return uint64(int((1 / 8) * 2 * _mojo_per_mogua))
     elif height < 6 * _blocks_per_year:
